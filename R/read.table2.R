5ff8a6ba0e91d4f4450cdaafaa504f1b9d562a00
Timothy Mak <tshmak@hku.hk>
2018-06-26 16:43:11 +0800

    Always requires data.table for reading text files

--- R/read.table2.R
+++ R/read.table2.R
@@ -1,9 +1,6 @@
-#' @title Function to read a text file depending on whether data.table is installed
+#' @title Function to read a text file 
 #' @keywords internal
-read.table2 <- function(file, header=F) {
-  if("data.table" %in% rownames(installed.packages())) {
-    return(data.table::fread(file, data.table=F, header=header))
-  } else {
-    return(read.table(file, header = header, stringsAsFactors = F))
-  }
+read.table2 <- function(file, header=F, data.table=F, check.names=TRUE, ...) {
+  return(data.table::fread(file, header=header, data.table=data.table, 
+                           check.names=check.names, ...))
 }
