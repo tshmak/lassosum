a6d7b5b67599975f56c7a75eea52c8cedd8f81fd
Timothy Mak <tshmak@hku.hk>
2018-08-21 10:34:16 +0800

    Initial standalone version

---
+++ inst/lassosum.R
@@ -0,0 +1,102 @@
+#!/usr/bin/env Rscript 
+Tim.load(lassosum)
+# library(lassosum)
+args <- parseargs()
+
+#### Options specific to standalone version ####
+# nthreads, data, n, pval, beta, OR, pheno, covar, out, lassosum.pipeline, 
+# pseudovalidate, validate, splitvalidate
+
+#### out ####
+if(is.null(args$out)) {
+  args$out <- "lassosum"
+} 
+
+#### trace ####
+if(!is.null(args$trace)) {
+  trace <- args$trace
+} else trace <- 1
+
+#### nthreads ####
+if(!is.null(args$nthreads)) {
+  cl <- parallel::makeForkCluster(args$nthreads)
+} else cl <- NULL
+
+#### data ####
+if(is.null(args$data)) {
+  stop("--data must be specified")
+}
+data <- read.table2(args$data, header=TRUE)
+
+#### variables in data ####
+vars <- c("cor", "chr", "pos", "snp", "A1", "A2", "LDblocks", 
+          "pval", "beta", "OR") # Other options specific to 
+for(v in (vars)) if(!is.null(args[[v]])) assign(v, data[[ args[[v]] ]]) else 
+  assign(v, NULL)
+
+# n is special, cos it can take a number or be a variable in data
+if(!is.null(args[["n"]])) n <- if(is.character(args[['n']])) data[[ args[['n']] ]] else 
+  args[['n']]
+if(is.null(beta) && !is.null(OR)) beta <- log(OR)
+
+#### p2cor ####
+if(is.null(cor)) {
+  if(!is.null(pval)) {
+    if(!is.null(n) && !is.null(beta)) {
+      cor <- p2cor(p, n=n, beta=beta)
+    } else {
+      stop("You need to specify n and beta/OR to convert pval to correlations.")
+    }
+  } else {
+    stop("Either cor or pval must be provided.")
+  }
+}
+
+#### data ####
+if(is.null(args$lassosum.pipeline)) {
+  message("Running lassosum.pipeline")
+  lp <- lassosum.pipeline(cor=cor, chr=chr, pos=pos, snp=snp, A1 = A1, A2=A2,
+                          test.bfile = args$test.bfile, ref.bfile=args$ref.bfile, 
+                          LDblocks=args$LDblocks, lambda=args$lambda, 
+                          s=args[['s']], destandardize=args$destandardize, 
+                          exclude.ambiguous = args$exclude.ambiguous, 
+                          keep.ref=args$keep.ref, remove.ref=args$remove.ref, 
+                          keep.test=args$keep.test, remove.test=args$remove.test,
+                          sample=args$sample, cluster=cl, 
+                          max.ref.bfile.n = args$max.ref.bfile.n, 
+                          trace=trace)
+  saveRDS(lp, file=paste0(out, ".lassosum.pipeline.rds"))
+} else {
+  lp <- readRDS(args$lassosum.pipeline)
+}
+
+#### pheno ####
+if(!is.null(args[['pheno']])) {
+  pheno <- read.table2(args[['pheno']])
+} else pheno <- NULL
+
+#### covar ####
+if(!is.null(args[['covar']])) {
+  covar <- read.table2(args[['covar']])
+} else covar <- NULL
+
+if(!is.null(pheno) || !is.null(args[['validate']])) {
+  message("Running validate.lassosum.pipeline")
+  v <- validate(lp, pheno=pheno, covar=covar, trace=trace)
+  saveRDS(v, file=paste0(out, ".validate.rds"))
+  write.table2(v$results.table, file=paste0(out, ".validate.results.txt"))
+}
+
+if(!is.null(pheno) || !is.null(args[['splitvalidate']])) {
+  message("Running splitvalidate.lassosum.pipeline")
+  v <- splitvalidate(lp, pheno=pheno, covar=covar, trace=trace)
+  saveRDS(v, file=paste0(out, ".splitvalidate.rds"))
+  write.table2(v$results.table, file=paste0(out, ".splitvalidate.results.txt"))
+}
+
+if(!is.null(args[['pseudovalidate']])) {
+  message("Running pseudovalidate.lassosum.pipeline")
+  v <- pseudovalidate(lp, trace=trace)
+  saveRDS(v, file=paste0(out, ".pseudovalidate.rds"))
+}
+
