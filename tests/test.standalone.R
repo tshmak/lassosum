11bb764e8e9f922138d87cdebc9e59235a63aeed
Timothy Mak <tshmak@hku.hk>
2018-08-21 12:22:44 +0800

    more tests

--- tests/test.standalone.R
+++ tests/test.standalone.R
@@ -5,11 +5,11 @@ system(paste("chmod +x", exec))
 setwd(system.file("tests", package="lassosum"))
 f <- function(file="") paste0(system.file("data", package="lassosum"), "/", file)
 ss <- read.table2(f("summarystats.txt"), header=TRUE)
-cmd <- parse.options(list(data=f("summarystats.txt"), chr="Chr", pos="Position", 
-                          A1="A1", A2="A2", pval="P_val", n=50000, OR="OR_A1", 
-                     test.bfile=f("testsample"), LDblocks="EUR.hg19", 
-                     validate=T, splitvalidate=T, pseudovalidate=T), 
-                     dot2dash=F)
+options <- list(data=f("summarystats.txt"), chr="Chr", pos="Position", 
+     A1="A1", A2="A2", pval="P_val", n=50000, OR="OR_A1", 
+     test.bfile=f("testsample"), LDblocks="EUR.hg19", 
+     validate=T, splitvalidate=T, pseudovalidate=T)
+cmd <- parse.options(options = options, dot2dash=F)
 # print(paste(exec, cmd))f
 # source(exec)
 system(paste(exec, cmd))
@@ -17,3 +17,8 @@ head(read.table2("lassosum.validate.results.txt", header=T))
 
 Tim.load(Rplink)
 fam <- read.fam(f("testsample"))
+fam2 <- fam[sample(nrow(fam), 50),]
+write.table2(fam2[,1:2], file=keep <- tempfile())
+options$keep.test <- keep
+system(paste(exec, parse.options(options = options, dot2dash=F)))
+lp <- readRDS("lassosum.lassosum.pipeline.rds")
