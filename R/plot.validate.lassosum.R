e93027568f8d59252acfad9a34eae1e1830047fc
Timothy Mak <tshmak@hku.hk>
2018-08-31 16:49:06 +0800

    More control over plotting

--- R/plot.validate.lassosum.R
+++ R/plot.validate.lassosum.R
@@ -1,18 +1,18 @@
-plot.validate.lassosum <- function(obj) {
+plot.validate.lassosum <- function(obj, legend.x="bottomright", log="x") {
   #' @title Plot function for \code{validate.lassosum} objects
   #' @export
   t <- obj$validation.table
   if(!any(is.finite(t$value))) return(invisible(NULL)) # If nothing to plot
   plot(unlist(t$lambda), 
        unlist(t$value), 
-       type="n", 
+       type="n", log=log,
        xlab="lambda", ylab=obj$validation.type)
   ss <- unique(t$s)
   for(i in 1:length(ss)) {
     subs <- subset(t, s == ss[i])
     points(subs$lambda, subs$value, col=i, type="o")
   }
-  legend(x="topright", col=1:length(ss), pch=1, 
+  legend(x=legend.x, col=1:length(ss), pch=1, 
          legend=paste0("s=", ss))
 
 }
