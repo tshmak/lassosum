b1e690381352571a152db059a9a83d0836e9b9be
Timothy Mak <tshmak@hku.hk>
2018-10-29 16:41:47 +0800

    Corrects wrong liftovers in hg38

---
+++ inst/data/Berisa.R
@@ -0,0 +1,19 @@
+Tmisc()
+setwd0("~/WORK/myRpackages/lassosum/inst/data/")
+for(pop in c("EUR", "ASN", "AFR")) {
+  # pop <- "EUR"
+  bed <- read.table.tim(paste("Berisa", pop, "hg19", "bed", sep="."))
+  l <- liftover(bed$chr, bed$start, bed$stop, from=19, to=38, bedformat = TRUE)
+  chrs <- unique(l$chr)
+  ll <- lapply(chrs, function(x) subset(l, chr==x))
+  lll <- list()
+  for(i in 1:length(ll)) {
+    # i <- 1
+    u <- unique(c(ll[[i]]$pos, ll[[i]]$pos2))
+    u <- sort(u[!is.na(u)])
+    lll[[i]] <- data.frame(chr=chrs[[i]], start=u[-length(u)], stop=u[-1])
+  }
+  new <- do.call(rbind, lll)
+  write.table.tim(new, file=paste("Berisa", pop, "hg38", "bed", sep="."))
+  
+}
